7af244b46ff1a0a411cdb3d6c3a15199dc0091d4
André Rosa Loiola <andre.kof@hotmail.com>
2024-09-10 19:38:15 -0300

    cartao

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import CreditCardForm from "../../components/Card";
+
+function Home() {
+  return (
+    <>
+      <div className="h-screen w-screen flex flex-col items-center justify-center bg-slate-200">
+        <CreditCardForm />
+      </div>
+    </>
+  );
+}
+
+export default Home;
